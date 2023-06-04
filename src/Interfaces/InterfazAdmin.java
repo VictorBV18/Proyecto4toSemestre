899d322493925ff47b6a51a88f24533cee682f1f
Seany-Aguilar <seanyaguilar24@hotmail.com>
2023-06-03 23:36:43 -0600

    Cambios en interfaz Administrador

--- src/Interfaces/InterfazAdmin.java
+++ src/Interfaces/InterfazAdmin.java
@@ -3,6 +3,8 @@ package Interfaces;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,26 +12,31 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class InterfazAdmin extends JFrame{
     
-                public JPanel panel;
-                public JLabel text;
         public InterfazAdmin() {
             
-         
-            
-            
-        this.setSize(500, 700);
+        this.setSize(900, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setTitle("T-REX INTERFAZ USUARIO ");
+        setTitle("( ◕▿◕ ) Administrador T-REX");
         setMinimumSize(new Dimension(500, 700));
         
-        panel = new JPanel();
-        panel.setBackground(Color.blue);
-        panel.setLayout(null);
-        this.getContentPane().add(panel);
-        
-       
-
     }
+        private void components(){
+         
+        //Fondo del panel    
+        JPanel fondoAdmin = new JPanel();
+        fondoAdmin.setSize(900, 600);
+        ImageIcon imageIcon = new ImageIcon("AdminF.png");
+        Image image = imageIcon.getImage().getScaledInstance(900, 600, Image.SCALE_SMOOTH);
+        JLabel background = new JLabel(new ImageIcon(image));
+        background.setBounds(0, 0, fondoAdmin.getWidth(), fondoAdmin.getHeight());
+        add(background);
+        fondoAdmin.setLayout(null);
+        
+        
+        
+        
+        
+        }
 }
     
